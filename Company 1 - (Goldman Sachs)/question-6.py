fd2e3634bd9e2183f53a82774d6e3bcf54f3c741
Parth Barse <91686761+ParthBarse@users.noreply.github.com>
2022-01-08 00:45:26 +0530

    Update question-6.py

--- Company 1 - (Goldman Sachs)/question-6.py
+++ Company 1 - (Goldman Sachs)/question-6.py
@@ -1,4 +1,4 @@
-# Question 5 - For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+# Question 6 - For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
 
 
 # Solution -
