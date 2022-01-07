2d025d4c7cfff8560715c0c44855a5833c6af5db
Parth Barse <91686761+ParthBarse@users.noreply.github.com>
2022-01-08 03:40:10 +0530

    Added question-12.py

---
+++ Company 1 - (Goldman Sachs)/question-12.py
@@ -0,0 +1,22 @@
+# Question 12 - Puzzle | Program to find number of squares in a chessboard
+
+
+# Solution -
+
+
+def countSquares(n):
+    return ((n * (n + 1) / 2) * (2 * n + 1) / 3)
+
+
+if __name__ == "__main__":
+
+    n = 4
+    print("Count if Squares is ", countSquares(n))
+
+
+
+# Thanks in advance if anyone is reviewing this code.
+# Program by - Parth Barse
+# Suggest me anything about this code on email - (parthbarse72@gmail.com) or InstaId - (https://www.instagram.com/parth.barse)
+
+#-------------------------------------------------------------------------------------
