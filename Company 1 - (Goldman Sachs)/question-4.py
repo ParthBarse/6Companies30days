d6e530116656d0406747da2adf8d6e7e263d0114
Parth Barse <91686761+ParthBarse@users.noreply.github.com>
2022-01-08 00:44:49 +0530

    Update question-4.py

--- Company 1 - (Goldman Sachs)/question-4.py
+++ Company 1 - (Goldman Sachs)/question-4.py
@@ -1,4 +1,4 @@
-# Given a string, Your task is to  complete the function encode that returns the run length encoded string for the given string.
+# Question 4 - Given a string, Your task is to  complete the function encode that returns the run length encoded string for the given string.
 # eg if the input string is “wwwwaaadexxxxxx”, then the function should return “w4a3d1e1x6″.
 
 
