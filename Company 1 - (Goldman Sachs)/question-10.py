acf7839fa3fbe59049dca70961b99e5dfb9a1051
Parth Barse <91686761+ParthBarse@users.noreply.github.com>
2022-01-08 03:41:24 +0530

    Added question-10.py

---
+++ Company 1 - (Goldman Sachs)/question-10.py
@@ -0,0 +1,19 @@
+# Question 10 - Find max 10 numbers in a list having 10M entries.
+
+
+# Solution -
+
+
+
+
+
+if __name__ == "__main__":
+    print()
+
+
+
+# Thanks in advance if anyone is reviewing this code.
+# Program by - Parth Barse
+# Suggest me anything about this code on email - (parthbarse72@gmail.com) or InstaId - (https://www.instagram.com/parth.barse)
+
+#-------------------------------------------------------------------------------------
